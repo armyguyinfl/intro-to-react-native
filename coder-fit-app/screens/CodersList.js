9cc160a8101af2d8b8bb238122939022c59b3b80
Daniel Kowalec <armyguyinfl@users.noreply.github.com>
2022-10-07 15:37:18 -0400

    Lab 7 complete. - Previously, added button to CodersList.js for first input. - Button element exists from lab 5-6. - Validated navigation UX is working as expected.

--- coder-fit-app/screens/CodersList.js
+++ coder-fit-app/screens/CodersList.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { useQuery, gql } from '@apollo/client'
 
 import { CODERS_QUERY } from '../queries'
