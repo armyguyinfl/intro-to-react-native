149e29b6f219ce284439f01734722562eea2e5c7
Daniel Kowalec <armyguyinfl@users.noreply.github.com>
2022-10-07 13:59:35 -0400

    Setup initial state to use with iOS and Android emulators. - Where Android localhost:4000 fix workaround will work with actual devices using ExpoGo app. - Add CodersList screen component. - Add ApolloClient, Setup Routing for Server with local system IP and port. Add Coders component to main App.js. - Install required packages @apollo/client and graphql for server.

--- coder-fit-app/App.js
+++ coder-fit-app/App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import CodersList from './screens/CodersList'
+
+const client = new ApolloClient({
+  // Below IP is only needed for Android or Actual Phone Devices on the same network.
+  // iOS Simular can handle using localhost with the ExpoGo app automatically
+  // Additionally, you can setup a static IP address for your computer mac address,
+  // therefore your IP address will never change and the code will always work on the same network
+
+  // localhost:4000 is our GraphQL server default localhost + port
+  // uri: 'http://localhost:4000',
+
+  // 192.168.1.10:4000 is our GraphQL server default {{local-ip}} + port
+  // uri: 'http://192.168.1.10:4000',
+
+  uri: 'http://192.168.1.10:4000',
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Daniel Open up App.js to start working on your app!</Text> */}
+      <ApolloProvider client={client}>
+        <CodersList />
+        <StatusBar style="auto" />
+      </ApolloProvider>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +40,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
